c66e9ae1a9a440697af76c935d888a4ce06aad79
blaineprogs <tura.dorothy@gmail.com>
2026-02-19 15:21:59 +0800

    PIT

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ const themeToggle = document.getElementById("themeToggle");
 
 themeToggle.addEventListener("click", function(){
 	document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeToggle.textContent = "Toggle Light Mode";
+    } else {
+        themeToggle.textContent = "Toggle Dark Mode";
+    }
 });
 
 const toggleSkills = document.getElementById("toggleSkills");
@@ -15,13 +21,14 @@ toggleSkills.addEventListener("click", function(){
 
 const submitBtn = document.getElementById("submitBtn");
 
-submitBtn.addEventListener("click",function(event){
-event.preventDefault();
-const name = document.getElementById("name").value;
-const email = document.getElementById("email").value;
-if (name===""||email===""){
-alert("Please fill in all required fields.");
-} else{
-alert("Form submitted successfully!");
-}
+submitBtn.addEventListener("click", function(event){
+	event.preventDefault();
+	const name = document.getElementById("name").value;
+	const email = document.getElementById("email").value;
+
+	if (name===""||email==="") {
+	  alert("Please fill in all required fields.");
+	} else {
+	  alert("Form submitted successfully!");
+	}
 });
